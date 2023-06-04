ced00ff591d2323e762f7061c96c462329e96e74
maorlayani <maorlayani@gmail.com>
2023-06-04 16:03:36 +0300

    fix bug in findByName func

--- api/company-data/companyData.service.ts
+++ api/company-data/companyData.service.ts
@@ -3,22 +3,14 @@ import { getCollection } from '../../services/db.service'
 import { ObjectId } from 'mongodb'
 // import { MY_BRAND_API_KEY, MY_BRAND_BASE_URL } from '../../private/privateKeys.service'
 
-// module.exports = {
-//     getByName,
-//     add,
-// }
-
 export async function getByName(companyName: string) {
     try {
         const collection = await getCollection('company_data')
-        console.log(companyName);
-
-        let company = await collection.findOne({ name: companyName.toLowerCase() })
-        console.log('From DB');
-        console.log(company);
+        const regex = new RegExp(companyName, 'i')
+        const regexTest = { $regex: regex }
+        let company = await collection.findOne({ name: regexTest })
         if (!company) {
-            console.log('New company');
-
+            // console.log('New company');
             company = await add(companyName)
         }
         return company
@@ -44,7 +36,14 @@ export async function add(companyName: string) {
         const collection = await getCollection('company_data')
         let companyData = await _getCompanyData(companyName, 'com')
         if (!companyData) companyData = await _getCompanyData(companyName, 'io')
-        // if (!companyData) return {}
+        if (!companyData) {
+            companyData = {
+                name: companyName,
+                description: '',
+                links: [],
+                logos: []
+            }
+        }
         const { insertedId } = await collection.insertOne(companyData)
         const addedCompany = await getById(insertedId)
         return addedCompany
@@ -57,12 +56,14 @@ async function _getCompanyData(companyName: string, domainExtension: string) {
     try {
         const apiData = await axios(
             `${process.env.MY_BRAND_BASE_URL}${companyName}.${domainExtension}`,
+            // `${MY_BRAND_BASE_URL}${companyName}.${domainExtension}`,
             {
                 headers: {
                     'Authorization': `Bearer ${process.env.MY_BRAND_API_KEY}`
+                    // 'Authorization': `Bearer ${MY_BRAND_API_KEY}`
                 }
             })
-        console.log('API CALL');
+        // console.log('API CALL');
         if (!apiData.data.name) return null
         apiData.data.name = apiData.data.name.toLowerCase()
         return apiData.data
