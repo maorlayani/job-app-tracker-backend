e1621c284a71f969c113c8816f2e0b20c248cad8
maorlayani <maorlayani@gmail.com>
2023-06-05 22:10:01 +0300

    update public folder

--- dist/server.js
+++ dist/server.js
@@ -4,24 +4,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// import path from 'path'
+const path_1 = __importDefault(require("path"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-// app.use(express.static('public'))
-// if (process.env.NODE_ENV === 'production') {
-// Express serve static files on production environment
-// app.use(express.static(path.resolve(__dirname, 'public')))
-// } else {
-// Configuring CORS 
-const corsOptions = {
-    // Make sure origin contains the url your frontend is running on
-    // origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://jobs-app-tracker.vercel.app/','*'],
-    origin: '*',
-    credentials: true
-};
-app.use((0, cors_1.default)(corsOptions));
-// }
+app.use(express_1.default.static('public'));
+if (process.env.NODE_ENV === 'production') {
+    // Express serve static files on production environment
+    app.use(express_1.default.static(path_1.default.resolve(__dirname, 'public')));
+}
+else {
+    // Configuring CORS 
+    const corsOptions = {
+        // Make sure origin contains the url your frontend is running on
+        // origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://jobs-app-tracker.vercel.app/','*'],
+        origin: '*',
+        credentials: true
+    };
+    app.use((0, cors_1.default)(corsOptions));
+}
 const tracker_routes_1 = __importDefault(require("./api/tracker/tracker.routes"));
 const companyData_routes_1 = __importDefault(require("./api/company-data/companyData.routes"));
 const technology_routes_1 = __importDefault(require("./api/technology/technology.routes"));
@@ -30,11 +31,11 @@ app.use('/api/tracker', tracker_routes_1.default);
 app.use('/api/company', companyData_routes_1.default);
 app.use('/api/technology', technology_routes_1.default);
 // Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/board/123 it will still respond with 
+// so when requesting http://localhost:3030/index.html/tracker/123 it will still respond with 
 // our SPA (single page app) (the index.html file) and allow react-router to take it from there
-// app.get('/**', (req: Request, res: Response) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, 'public', 'index.html'));
+});
 const port = process.env.PORT || 3030;
 app.listen(port, () => console.log('Server running at port: ' + port));
 // module.exports = app;
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,26 @@
 import express, { Application, Request, Response } from 'express'
-// import path from 'path'
+import path from 'path'
 import cors from 'cors'
 
 const app: Application = express()
 
 app.use(express.json())
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
 
-// if (process.env.NODE_ENV === 'production') {
-// Express serve static files on production environment
-// app.use(express.static(path.resolve(__dirname, 'public')))
-// } else {
-// Configuring CORS 
-const corsOptions = {
-    // Make sure origin contains the url your frontend is running on
-    // origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://jobs-app-tracker.vercel.app/','*'],
-    origin: '*',
-    credentials: true
+if (process.env.NODE_ENV === 'production') {
+    // Express serve static files on production environment
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    // Configuring CORS 
+    const corsOptions = {
+        // Make sure origin contains the url your frontend is running on
+        // origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://jobs-app-tracker.vercel.app/','*'],
+        origin: '*',
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 }
-app.use(cors(corsOptions))
-// }
 import applicationsRoutes from './api/tracker/tracker.routes'
 import companysDatasRoutes from './api/company-data/companyData.routes'
 import technologiesRoutes from './api/technology/technology.routes'
@@ -31,11 +31,11 @@ app.use('/api/company', companysDatasRoutes)
 app.use('/api/technology', technologiesRoutes)
 
 // Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/board/123 it will still respond with 
+// so when requesting http://localhost:3030/index.html/tracker/123 it will still respond with 
 // our SPA (single page app) (the index.html file) and allow react-router to take it from there
-// app.get('/**', (req: Request, res: Response) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const port = process.env.PORT || 3030
 app.listen(port, () => console.log('Server running at port: ' + port))
