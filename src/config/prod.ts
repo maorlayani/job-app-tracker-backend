eb8861a2e053837ae5c24eae25e8531c302f2e08
maorlayani <maorlayani@gmail.com>
2023-06-05 12:27:04 +0300

    update files structure

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -5,5 +5,5 @@
 
 
 const dbURL = `mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@cluster0.huupjkb.mongodb.net/?retryWrites=true&w=majority`
-// export default process.env.MONGODB_URI
-export default dbURL
+// export default dbURL
+export default process.env.MONGODB_URI
