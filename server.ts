f2d0c830500612403403b41e471117fb8af558ee
maorlayani <maorlayani@gmail.com>
2023-06-05 10:41:52 +0300

    update sever and vercel files

--- server.ts
+++ server.ts
@@ -1,17 +1,17 @@
 import express, { Application, Request, Response } from 'express'
-// import path from 'path'
+import path from 'path'
 
 import cors from 'cors'
 
 const app: Application = express()
 
 app.use(express.json())
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
 
 if (process.env.NODE_ENV === 'production') {
     // Express serve static files on production environment
-    // app.use(express.static(path.resolve(__dirname, 'public')))
+    app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     // Configuring CORS 
     const corsOptions = {
@@ -29,6 +29,13 @@ import technologiesRoutes from './api/technology/technology.routes'
 app.use('/api/tracker', applicationsRoutes)
 app.use('/api/company', companysDatasRoutes)
 app.use('/api/technology', technologiesRoutes)
+
+// Make every server-side-route to match the index.html
+// so when requesting http://localhost:3030/index.html/board/123 it will still respond with
+// our SPA (single page app) (the index.html file) and allow react-router to take it from there
+app.get('/**', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const port = process.env.PORT || 3030
 app.listen(port, () => console.log('Server running at port: ' + port))
